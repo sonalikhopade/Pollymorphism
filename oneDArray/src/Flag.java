00cab43b9054166cd15b13ceb1c769b8fa49ab1c
sonalikhopade <sonalikhopade05@gmail.com>
2017-10-04 11:31:41 +0530

    first commit

---
+++ oneDArray/src/Flag.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Flag {
+    
+    public static void main(String[] args) {
+        row=3;col=4;
+        for(int i=0; i<=8; i++)
+        {
+            
+           
+            System.out.println("*");
+        }
+    }
+    
+}
---
+++ oneDArray/src/TwoDArray.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class TwoDArray {
+    public static void main(String[] args) {
+       
+      int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
+        for(int i=0; i<3; i++)
+            
+        {
+           // System.out.println("");
+            for(int j=0;j<3;j++)
+            {
+                System.out.print(" " +arr[i][j]);
+            }
+           System.out.println("");
+        }
+    }
+    
+}
